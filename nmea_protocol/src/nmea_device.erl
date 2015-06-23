42799e54a049241546a3973b48c17e76028ccb85
Sergey Loguntsov <loguntsov@gmail.com>
2015-06-23 16:35:39 +0300

    NMEA protocol implementation template

---
+++ nmea_protocol/src/nmea_device.erl
@@ -0,0 +1,20 @@
+-module(nmea_device).
+-author("Sergey Loguntsov").
+
+
+-behaviour(cl_device).
+-export([
+  new/2, handle_update_info/2
+]).
+
+new(Id, Data) ->
+  Imei = maps:get(imei, Data),
+  true = is_integer(Imei),
+  Default = #{
+  },
+  cl_device:new_internal(Id, ?MODULE, #{
+    imei => Imei,
+    login => integer_to_binary(Imei)
+  }, Default).
+
+handle_update_info(_Info, _Device) -> undefined.
---
+++ nmea_protocol/src/nmea_protocol_app.erl
@@ -0,0 +1,43 @@
+-module(nmea_protocol_app).
+-author("Sergey Loguntsov").
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2,
+  stop/1]).
+
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
+
+-spec(start(StartType :: normal | {takeover, node()} | {failover, node()},
+    StartArgs :: term()) ->
+  {ok, pid()} |
+  {ok, pid(), State :: term()} |
+  {error, Reason :: term()}).
+start(_StartType, _StartArgs) ->
+  calypso_listener:register_protocol(nmea_protocol),
+  Pid = spawn_link(fun() ->
+    calypso_starter:empty_loop(fun() ->
+      calypso_listener:register_protocol(nmea_protocol)
+    end)
+  end),
+  { ok, Pid }.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(stop(State :: term()) -> term()).
+stop(_State) ->
+  ok.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
